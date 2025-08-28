38433de3c8c965f531c40963d7e5fab3aa24f635
James Anderson <james.anderson@wright.edu>
2025-08-28 10:04:13 -0400

    added code example introducing arrays in C++

---
+++ inClassCodeExamples/arrays/main.cpp
@@ -0,0 +1,28 @@
+/**
+ * CS 3100
+ * In class C++ examples
+ * from https://en.cppreference.com/w/cpp/language/array.html
+ *  A declaration of the form T a[N]; declares a as an array object that consists of N
+ *  contiguously allocated objects of type T
+ */
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    const int SIZE = 5;
+    int arr[SIZE]; // we will discuss this, but SIZE ***MUST*** be a constant (or int literal)
+
+    // before assigning indexes, the array will be full of garbage values
+    for (int i = 0; i < SIZE; i++) {
+        arr[i] = (1 + i) * 100;
+    }
+
+    // I can make the loop go more times than SIZE
+    // C++ doesn't stop me from accessing off the end of the array
+    for (int i = 0; i < SIZE; i++) {
+        cout << arr[i] << endl;
+    }
+
+    return 0;
+}
