baa6569be16545a2f931bda2ac31a5f2203aad24
James Anderson <james.anderson@wright.edu>
2025-09-11 10:21:46 -0400

    added vector examples to dynamicMemory example

--- inClassCodeExamples/dynamicMemory/main.cpp
+++ inClassCodeExamples/dynamicMemory/main.cpp
@@ -7,9 +7,12 @@
  * This file contains examples of how NOT to make a dynamic array on the stack. Then it shows the correct way
  * to use the 'new' keyword to allocate memory on the heap. In addition, because we use 'new' to allocate memory,
  * it is necessary to use 'delete' to release/free the memory back to the OS. Note that because we use 'new' to
- * allocate memory for an array of things, we need to use 'delete[]' when releasing memory.
+ * allocate memory for an array of things, we need to use 'delete[]' when releasing memory. Also at the end a brief
+ * example of using the C++ vector object, which is similar to ArrayList in Java that will automatically resize our array
  */
+
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -26,9 +29,11 @@ int main() {
     char* dynArray = nullptr; // if we aren't initializing a pointer with an address it's good practice to
                                 // initialize it to 'nullptr' so we don't accidentally try to access invalid memory
 
+    // cout << *dynArray << endl;
+
     cout << "Before loop..." << endl; // to demonstrate memory leak (or lack of one) place a break point here and observe
                                         // how much memory your program is utilizing
-    for (int i = 0; i < 100000; i++) {
+    for (int i = 0; i < 10; i++) {
         dynArray = new char[N]; // the 'new' keyword asks the OS for memory on the heap to store the requested data
                                     // the return from using 'new' is the address to where that memory is
         char ch = 'A';
@@ -40,7 +45,16 @@ int main() {
         delete[] dynArray; // tell the OS we are done with the memory we asked for when we used 'new'
                             // this does NOT affect what address is stored in dynArray, it still is pointing
                             // to the same place, only we have just released that memory
-        dynArray[1] = 'Q';  // even though we released the memory dynArray pointed to, C++ doesn't stop us
+        dynArray = nullptr; // to be safe we want to set dynArray to nullptr to prevent us from
+                            // trying to access invalid memory later
+
+        if (dynArray != nullptr) { // we can also check if dynArray is not null with: if (dynArray)
+            dynArray[3] = '7';
+        }
+        else {
+            cout << "dynArray is nullptr" << endl;
+        }
+        //dynArray[1] = 'Q';  // even though we released the memory dynArray pointed to, C++ doesn't stop us
                             // from accessing/modifying it, similar to how we can access array elements out-of-bounds
     }
     cout << "After loop..." << endl; // place a breakpoint here to observe how much memory the program is utilizing
@@ -53,6 +67,35 @@ int main() {
     }
     // even though test isn't in a loop/if/function, it is only available in the scope of the curly braces and not here
     //cout << test;
+
+    /////////////////////////////////////////////
+    /// vector examples
+    /// we need to #include <vector> at the beginning of the file
+
+    vector<int> v; // declare an empty vector that can store ints
+
+    // add 0, 1, 2... to back of vector
+    for (int i = 0; i < 10; i++) {
+        v.push_back(i);
+    }
+
+    // we can use v[index] or v.at(index)
+    cout << v.at(1) << endl;
+    cout << v[1] << endl;
+
+    // the difference is v[index] does not throw an exception for out of bounds
+    // v.at(index) does throw an exception
+    //cout << v[20] << endl;
+    //cout << v.at(20);
+
+    // many other methods:
+    cout << v.front() << endl; // front() gets first element
+    cout << v.back() << endl;  // back() gets last element
+    v.pop_back();   // pop_back() removes last element
+    cout << v.back() << endl;
+    cout << v.data() << endl; // get the int pointer to the array
+    cout << v.size() << endl; // get number of elements in array
+    v.clear(); // resets vector to size of zero
 
     return 0;
 }
